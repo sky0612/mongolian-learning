195b5b0de0d17904879c85b0c360e8fe693c2b99
sky0612 <rzp5mgvmzk@privaterelay.appleid.com>
2026-06-29 19:43:21 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-self.addEventListener('install',e=>e.waitUntil(caches.open('mn-final').then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
+self.addEventListener('fetch',()=>{});
