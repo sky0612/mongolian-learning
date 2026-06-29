f5899c893a379b7801a9862b43c6df8cdd9f5568
sky0612 <rzp5mgvmzk@privaterelay.appleid.com>
2026-06-29 19:06:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-const CACHE='mn-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
+const CACHE='mongolian-pwa-v1';
+self.addEventListener('install',e=>{
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])));
+});
+self.addEventListener('fetch',e=>{
+ e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+});
